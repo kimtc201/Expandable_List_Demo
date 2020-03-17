33f758c09a84235529d8493de4e34f5621955cb4
tckim <tckim@hconnect.co.kr>
2020-03-17 15:55:08 +0900

    My JSON Server Repo 수정

--- app/src/main/java/com/github/tckim/expandable_list_demo/network/contract.kt
+++ app/src/main/java/com/github/tckim/expandable_list_demo/network/contract.kt
@@ -2,7 +2,7 @@ package com.github.tckim.expandable_list_demo.network
 
 object EndPoint{
     /** 목록 조회 */
-    const val GET_PART = "kimtc201/tckim/lists"
+    const val GET_PART = "kimtc201/Expandable_List_Demo/lists"
     /** 부분 결과 조회 */
-    const val GET_PART_RESULT = "kimtc201/tckim/parts"
+    const val GET_PART_RESULT = "kimtc201/Expandable_List_Demo/parts"
 }
